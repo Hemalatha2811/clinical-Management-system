e3c9bd5268c6a5030c5290d355db2aff49496014
Hemalatha2811 <100455881+Hemalatha2811@users.noreply.github.com>
2022-03-04 12:42:43 +0530

    Create Patient.cs

---
+++ Patient.cs
@@ -0,0 +1,28 @@
+   using System;
+    using System.Collections.Generic;
+    
+    public partial class Patient
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Patient()
+        {
+            this.Appointments = new HashSet<Appointment>();
+            this.Histories = new HashSet<History>();
+            this.Prescriptions = new HashSet<Prescription>();
+        }
+    
+        public int PatientId { get; set; }
+        public Nullable<int> Id { get; set; }
+        public string Name { get; set; }
+        public Nullable<int> Age { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Purpose { get; set; }
+    
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Appointment> Appointments { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<History> Histories { get; set; }
+        public virtual login login { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Prescription> Prescriptions { get; set; }
+    }
